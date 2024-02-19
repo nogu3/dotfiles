b73da524ca33e21c6b5e29770275f0ee8674ef4c
shoya.noguchi <nogukids@gmail.com>
2024-02-19 21:15:29 +0000

    add lazygit and delta

--- init.sh
+++ init.sh
@@ -1,2 +1,8 @@
 # tmux
-ln -s "`pwd`/.tmux.conf" ~
+rm -f ~/.tmux.conf
+ln -s "`pwd`/settings/tmux/.tmux.conf" ~/.tmux.conf
+
+# lazygit
+mkdir -p ~/.config/lazygit
+rm -f ~/.config/lazygit/config.yml
+ln -s "`pwd`/settings/lazygit/config.yml" ~/.config/lazygit/config.yml
