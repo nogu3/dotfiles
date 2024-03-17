5ee5dd6aa1a268de044f869a997af3c0dc18c903
shoya.noguchi <nogukids@gmail.com>
2024-03-17 11:21:08 +0000

    refactor keymap

--- settings/nvim/lua/keymap.lua
+++ settings/nvim/lua/keymap.lua
@@ -1,45 +1,45 @@
 -- keymap
+local keyset = vim.keymap.set
 -- autocomplete enter
-vim.keymap.set('i', '<CR>', 'coc#pum#visible() ? coc#pum#confirm() : "<CR>"', {expr = true})
+keyset('i', '<CR>', 'coc#pum#visible() ? coc#pum#confirm() : "<CR>"', {expr = true})
 local opts = {silent = true, noremap = true, expr = true, replace_keycodes = false}
-vim.keymap.set("i", "<Down>", 'coc#pum#visible() ? coc#pum#next(1) : "<Down>"', opts)
-vim.keymap.set("i", "<Up>", 'coc#pum#visible() ? coc#pum#prev(1) : "<Up>"', opts)
-vim.keymap.set("i", "<c-space>", "coc#refresh()", {silent = true, expr = true})
+keyset("i", "<Down>", 'coc#pum#visible() ? coc#pum#next(1) : "<Down>"', opts)
+keyset("i", "<Up>", 'coc#pum#visible() ? coc#pum#prev(1) : "<Up>"', opts)
+keyset("i", "<c-space>", "coc#refresh()", {silent = true, expr = true})
 
 -- copy current to mark
-vim.keymap.set("n", "cm", "y'm<CR>")
+keyset("n", "cm", "y'm<CR>")
 -- delete current to mark
-vim.keymap.set("n", "dm", "d'm<CR>")
+keyset("n", "dm", "d'm<CR>")
 
 -- esc
-vim.keymap.set('i', 'jj', '<Esc>')
+keyset('i', 'jj', '<Esc>')
 
 -- control buffer
-vim.keymap.set("n", "<leader>k", ":bnext<CR>")
-vim.keymap.set("n", "<leader>j", ":bprev<CR>")
-vim.keymap.set("n", "<leader>x", ":bw<CR>")
-vim.keymap.set("n", "<leader>xx", ":bw!<CR>")
-vim.keymap.set("n", "<leader>ax", ":%bd<CR>")
+keyset("n", "<leader>k", ":bnext<CR>")
+keyset("n", "<leader>j", ":bprev<CR>")
+keyset("n", "<leader>x", ":bw<CR>")
+keyset("n", "<leader>xx", ":bw!<CR>")
+keyset("n", "<leader>ax", ":%bd<CR>")
 -- create empty file
-vim.keymap.set("n", "<leader>n", ":tabnew<CR>")
+keyset("n", "<leader>n", ":tabnew<CR>")
 
 -- copy clipboard relative file path
-vim.keymap.set("n", "<leader>fp", ":let @+ = expand('%')<CR>")
-
+keyset("n", "<leader>fp", ":let @+ = expand('%')<CR>")
 
 -- telescope
 local builtin = require('telescope.builtin')
 -- find file
-vim.keymap.set('n', '<leader>p', builtin.find_files)
+keyset('n', '<leader>p', builtin.find_files)
 -- grep file
-vim.keymap.set('n', '<leader>ff', builtin.live_grep)
+keyset('n', '<leader>ff', builtin.live_grep)
 
 -- telescope file browser
 require("telescope").load_extension "file_browser"
 -- show file browser
-vim.keymap.set('n', '<leader>fb', ":Telescope file_browser<CR>")
+keyset('n', '<leader>fb', ":Telescope file_browser<CR>")
 -- search config dir
-vim.keymap.set('n', '<leader>nv', ":Telescope find_files cwd=~/.config/nvim<CR>")
+keyset('n', '<leader>nv', ":Telescope find_files cwd=~/.config/nvim<CR>")
 
 -- toggleterm
 local Terminal = require("toggleterm.terminal").Terminal
@@ -51,17 +51,17 @@ local lazygit = Terminal:new({
 
 -- lazygit
 function _lazygit_toggle()
-	lazygit:toggle()
+    lazygit:toggle()
 end
 
-vim.keymap.set("n", "<leader>lg", "<cmd>lua _lazygit_toggle()<CR>")
+keyset("n", "<leader>lg", "<cmd>lua _lazygit_toggle()<CR>")
 
 -- ale
-vim.keymap.set("n", "<leader>ft", ":ALEFix<CR>")
+keyset("n", "<leader>ft", ":ALEFix<CR>")
 
 -- coc.nvim_set_keymap
-vim.keymap.set('n', 'gd', '<Plug>(coc-definition)')
+keyset('n', 'gd', '<Plug>(coc-definition)')
 
 -- OpenGithubFile
-vim.keymap.set('n', '<leader>og', ':OpenGithubFile<CR>)')
+keyset('n', '<leader>og', ':OpenGithubFile<CR>)')
 
