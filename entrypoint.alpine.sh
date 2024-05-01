e85671c6d60b889d3f15d2a696d164ebeda950af
shoya.noguchi <nogukids@gmail.com>
2024-05-01 13:18:08 +0000

    migrate alpine

--- entrypoint.alpine.sh
+++ entrypoint.alpine.sh
@@ -10,7 +10,9 @@ USER_ID=${HOST_UID:-1000}
 GROUP_ID=${HOST_GID:-1000}
 
 usermod -u $USER_ID -o $USER_NAME > /dev/null
-groupmod -g $GROUP_ID $GROUP_NAME
+groupmod -o -g $GROUP_ID $GROUP_NAME
+
+chown $USER_NAME:$GROUP_NAME /workspaces/src
 
 su-exec $USER_NAME /tmp/codecraft/init.sh
 
