29d02b166f0045d215235544ff87e73fe58f58c5
shoya.noguchi <nogukids@gmail.com>
2024-05-11 05:18:23 +0000

    add shortcut relative file path and fix buffer

--- settings/lazyvim/lua/config/keymaps.lua
+++ settings/lazyvim/lua/config/keymaps.lua
@@ -20,15 +20,22 @@ map("n", "dm", "d'm<cr><esc>", silent("Delete to mark"))
 map("n", "<leader>go", ":OpenInGHFile<cr>", silent("Open github url to current file"))
 map("n", "<leader>gl", ":OpenInGHFileLines<cr>", silent("Open github url to current file with line"))
 
+-- copy relative file path
+map("n", "<leader>fp", ":let @+ = expand('%')<cr>", silent("Copy relative file path"))
+
 -- save
 map({ "x", "n", "s" }, "<leader>cs", "<cmd>w<cr><esc>", silent("Save File"))
 
 -- select all
 map("n", "<leader>a", "gg<S-v>G", silent("Select All"))
 
--- control buffer
+-- buffer
 map("n", "<leader>k", ":bnext<cr>", silent("Next Buffer"))
 map("n", "<leader>j", ":bprev<cr>", silent("Previous Buffer"))
-map("n", "<leader>x", ":bw<cr>", silent("Quit Buffer"))
-map("n", "<leader>xx", ":bw!<cr>", silent("Force Quit Buffer"))
-map("n", "<leader>ax", ":%bd<cr>", silent("All Quit Buffer"))
+map("n", "<leader>ba", ":%bd<cr>", silent("Delete All Buffers"))
+
+-- nvim
+-- checkhealth
+map("n", "<leader>nc", ":checkhealth<cr>", silent("Checkhealth"))
+-- reload config
+map("n", "<leader>ns", ":source %<cr>", silent("Reload config"))
