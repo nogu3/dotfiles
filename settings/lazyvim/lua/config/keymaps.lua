e3924bb7233ecfc7d1709f25de0a281bdae27281
shoya.noguchi <nogukids@gmail.com>
2024-05-11 06:26:47 +0000

    format keymaps

--- settings/lazyvim/lua/config/keymaps.lua
+++ settings/lazyvim/lua/config/keymaps.lua
@@ -12,19 +12,10 @@ local function silent(desc)
   }
 end
 
+-- general
 -- mark
 map("n", "cm", "y'm<cr><esc>", silent("Copy to mark"))
 map("n", "dm", "d'm<cr><esc>", silent("Delete to mark"))
-
--- open github url
-map("n", "<leader>go", ":OpenInGHFile<cr>", silent("Open github url to current file"))
-map("n", "<leader>gl", ":OpenInGHFileLines<cr>", silent("Open github url to current file with line"))
-
--- copy relative file path
-map("n", "<leader>fp", ":let @+ = expand('%')<cr>", silent("Copy relative file path"))
-
--- save
-map({ "x", "n", "s" }, "<leader>cs", "<cmd>w<cr><esc>", silent("Save File"))
 
 -- select all
 map("n", "<leader>a", "gg<S-v>G", silent("Select All"))
@@ -34,8 +25,20 @@ map("n", "<S-j>", ":bprev<cr>", silent("Prev Buffer"))
 map("n", "<S-l>", ":bnext<cr>", silent("Next Buffer"))
 map("n", "<leader>ba", ":%bd<cr>", silent("Delete All Buffers"))
 
+-- git
+-- open github url
+map("n", "<leader>go", ":OpenInGHFile<cr>", silent("Open github url to current file"))
+map("n", "<leader>gl", ":OpenInGHFileLines<cr>", silent("Open github url to current file with line"))
+
+-- file
+-- copy relative file path
+map("n", "<leader>fp", ":let @+ = expand('%')<cr>", silent("Copy relative file path"))
+
+-- code
+-- save
+map({ "x", "n", "s" }, "<leader>cs", "<cmd>w<cr><esc>", silent("Save File"))
+
 -- nvim
 -- checkhealth
 map("n", "<leader>nc", ":checkhealth<cr>", silent("Checkhealth"))
--- reload config
-map("n", "<leader>ns", ":source %<cr>", silent("Reload config"))
+
