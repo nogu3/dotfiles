ce5298535f8d36dba00eaf093e24542ded94bc27
shoya.noguchi <nogukids@gmail.com>
2024-05-11 05:55:33 +0000

    fix ui.lua

--- settings/lazyvim/lua/plugins/ui.lua
+++ settings/lazyvim/lua/plugins/ui.lua
@@ -1,8 +1,12 @@
 return {
-  "folke/which-key.nvim",
-  opts = function(_, opts)
-    if LazyVim.has("noice.nvim") then
-      opts.defaults["<leader>n"] = { name = "+neovim" }
-    end
-  end,
+
+  {
+    "folke/which-key.nvim",
+    opts = function(_, opts)
+      if LazyVim.has("noice.nvim") then
+        opts.defaults["<leader>n"] = { name = "+neovim" }
+      end
+    end,
+  },
+
 }
