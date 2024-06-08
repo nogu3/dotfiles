7ac18565d37cf0a2228dfbddffda38ab5d20354a
shoya.noguchi <nogukids@gmail.com>
2024-06-09 07:41:22 +0900

    add general settings

--- settings/zenvim/lua/config/options.lua
+++ settings/zenvim/lua/config/options.lua
@@ -4,3 +4,48 @@ vim.g.maplocalleader = "\\"
 
 local opt = vim.opt
 opt.timeoutlen = 300
+
+-- not create backup file
+opt.backup = false
+-- not create swap file
+opt.swapfile = false
+-- auto reload when File being edited changed
+opt.autoread = true
+-- open file when File being edited exist
+opt.hidden = true
+-- show command on Status line when Command being entered
+opt.showcmd = true
+-- show mode is off. Because unnessary.
+opt.showmode = false
+-- tab to space
+opt.expandtab = true
+
+-- Strong cursol line
+opt.cursorline = true
+
+-- show line number
+opt.number = true
+-- convert tabs to spaces
+opt.tabstop = 2
+-- space count when auto indent
+opt.shiftwidth = 2
+-- truecolor support
+opt.termguicolors = true
+
+-- Search ignore lower or upper when search str contain lower case
+opt.ignorecase = true
+-- Search case sensitive when search str contain upper case
+opt.smartcase = true
+
+-- Set up how to show hidden characters
+opt.listchars = {
+	tab = "»-", -- Show a tab as '»-' on the screen
+	trail = "-", -- Show spaces at the end of a line as '-'
+	eol = "↲", -- Show the end of a line as '↲'
+	extends = "»", -- Show '»' when a line is too long and continues off the screen
+	precedes = "«", -- Show '«' when a line starts off the screen
+	nbsp = "%", -- Show a space that doesn't break into a new line as '%'
+}
+
+-- Turn on the setting to see the hidden characters
+vim.wo.list = true
