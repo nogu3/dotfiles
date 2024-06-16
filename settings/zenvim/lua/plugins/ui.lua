dd616ed1ff8f3b8c07d501716f63c45e284000f7
shoya.noguchi <nogukids@gmail.com>
2024-06-16 20:08:27 +0900

    fix c-p

--- settings/zenvim/lua/plugins/ui.lua
+++ settings/zenvim/lua/plugins/ui.lua
@@ -107,8 +107,7 @@ return {
     "akinsho/bufferline.nvim",
     event = "VeryLazy",
     keys = {
-      -- FIXME Not Working
-      -- { "<C-p>", "<cmd>bprev<cr>", desc = "Prev Buffer" },
+      { "<C-p>", "<cmd>bprev<cr>", desc = "Prev Buffer" },
       { "<C-n>", "<cmd>bnext<cr>", desc = "Next Buffer" },
       { "<leader>d", "<cmd>bd<cr>", desc = "Delete Buffer" },
       { "<leader>D", "<cmd>bd!<cr>", desc = "Force Delete Buffer" },
