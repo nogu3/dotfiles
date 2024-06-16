e6f1e0146cafdff743674a5e1a69556547bd1a26
shoya.noguchi <nogukids@gmail.com>
2024-06-16 06:18:54 +0000

    invalid lualine when copilot.lua

--- settings/zenvim/lua/plugins/extras/copilot.lua
+++ settings/zenvim/lua/plugins/extras/copilot.lua
@@ -2,31 +2,34 @@ return {
   {
     "github/copilot.vim",
   },
-  {
-    "nvim-lualine/lualine.nvim",
-    optional = true,
-    event = "VeryLazy",
-    opts = function(_, opts)
-      require("plenary.async").run(function()
-        return vim.cmd("Copilot status") == "Copilot: Ready"
-      end, function(is_enabled_copilot)
-        table.insert(opts.sections.lualine_x, 2, {
-          function()
-            if is_enabled_copilot then
-              return " "
-            else
-              return ":"
-            end
-          end,
-          color = function()
-            if is_enabled_copilot then
-              return { fg = 255 }
-            else
-              return { fg = 241 }
-            end
-          end,
-        })
-      end)
-    end,
-  },
+  -- FIXME lazy load when run nvim
+  -- {
+  --   "nvim-lualine/lualine.nvim",
+  --   optional = true,
+  --   event = "VeryLazy",
+  --   opts = function(_, opts)
+  --     require("plenary.async").run(function()
+  --       FIXME vim.fn.execute is blocking function.
+  --       local result = vim.fn.execute("Copilot status")
+  --       return string.find(result, "Ready") ~= nil
+  --     end, function(is_enabled_copilot)
+  --       table.insert(opts.sections.lualine_x, 2, {
+  --         function()
+  --           if is_enabled_copilot then
+  --             return " "
+  --           else
+  --             return ":"
+  --           end
+  --         end,
+  --         color = function()
+  --           if is_enabled_copilot then
+  --             return { fg = 255 }
+  --           else
+  --             return { fg = 241 }
+  --           end
+  --         end,
+  --       })
+  --     end)
+  --   end,
+  -- },
 }
