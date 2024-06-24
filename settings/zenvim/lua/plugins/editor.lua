f48be9d77917e30f4a3764cdbde6e2d5d7c1a544
shoya.noguchi <nogukids@gmail.com>
2024-06-24 12:30:49 +0000

    add opt for toggleterm

--- settings/zenvim/lua/plugins/editor.lua
+++ settings/zenvim/lua/plugins/editor.lua
@@ -130,6 +130,15 @@ return {
         cmd = "lazygit",
         direction = "float",
         hidden = true,
+        float_opts = {
+          border = "curved",
+          width = function()
+            return math.floor(vim.o.columns * 0.9)
+          end,
+          height = function()
+            return math.floor(vim.o.lines * 0.9)
+          end,
+        },
       })
 
       Zenvim.keymap_silent("n", "<leader><Space>", function()
