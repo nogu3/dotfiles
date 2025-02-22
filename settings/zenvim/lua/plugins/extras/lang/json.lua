6415221b7700fc59ea696c25e333d425ff2cef93
shoya.noguchi <nogukids@gmail.com>
2025-02-22 16:58:41 +0900

    fix json.lua

--- settings/zenvim/lua/plugins/extras/lang/json.lua
+++ settings/zenvim/lua/plugins/extras/lang/json.lua
@@ -5,38 +5,37 @@ return {
     opts = function(_, opts)
       opts.ensure_installed = Zenvim.list_extend_with_nil(opts.ensure_installed, {
         "json",
-        "json5",
-        "JSON with comments",
+        "jsonc",
       })
     end,
   },
 
-  -- HACK one method define
   -- lsp install
   {
     "williamboman/mason.nvim",
     optional = true,
     opts = function(_, opts)
       Zenvim.list_extend_with_nil(opts.ensure_installed, {
-        "prettier",
+        "json-lsp",
       })
     end,
   },
 
   -- lsp config
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   opts = function(_, opts)
-  --     opts.formatters_by_ft = Zenvim.list_append_with_nil(opts.formatters_by_ft, "jsonls", {})
-  --   end,
-  -- },
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      opts.formatters_by_ft = Zenvim.list_append_with_nil(opts.formatters_by_ft, "jsonls", {})
+    end,
+  },
 
   -- formatter
   {
     "stevearc/conform.nvim",
     optional = true,
     opts = function(_, opts)
-      opts.formatters_by_ft = Zenvim.list_append_with_nil(opts.formatters_by_ft, "json", { "prettier" })
+      opts.formatters_by_ft = Zenvim.list_append_with_nil(opts.formatters_by_ft, "json", { "jsonls" })
+      opts.formatters_by_ft = Zenvim.list_append_with_nil(opts.formatters_by_ft, "jsonc", { "jsonls" })
     end,
   },
 
@@ -45,7 +44,8 @@ return {
     "mfussenegger/nvim-lint",
     optional = true,
     opts = function(_, opts)
-      opts.linters_by_ft = Zenvim.list_append_with_nil(opts.linters_by_ft, "json", { "prettier" })
+      opts.linters_by_ft = Zenvim.list_append_with_nil(opts.linters_by_ft, "json", { "jsonls" })
+      opts.linters_by_ft = Zenvim.list_append_with_nil(opts.linters_by_ft, "jsonc", { "jsonls" })
     end,
   },
 }
