1cf9dab504bac40bb12e476278fba0d4c501c647
shoya.noguchi <nogukids@gmail.com>
2025-03-01 16:51:11 +0900

    migrate ghurl

--- settings/zenvim/lua/config/keymaps.lua
+++ settings/zenvim/lua/config/keymaps.lua
@@ -92,17 +92,11 @@ Zenvim.keymap_silent("n", "<leader>gl", function()
 end, "Copy github url to clipboard")
 
 Zenvim.keymap_silent("n", "<leader>gp", function()
-  -- TODO move to ghurl. add -p option
-  -- TODO get remote url without gh command.
-  local url = vim.fn.system("git config --get remote.origin.url")
-  url = url:gsub("%s+", "")
-  url = url:gsub("%.git$", "")
-
   if Zenvim.pr_number == nil then
     vim.notify("Warning: PR number not found", vim.log.levels.WARN)
     return
   end
 
-  local pr_url = url .. "/pull/".. Zenvim.pr_number
+  local pr_url = vim.fn.system("ghurl pr " .. Zenvim.pr_number)
   Zenvim.set_register_with_print_message("+", pr_url, "Copy github pr link to clipboard")
 end, "Copy github pr link to clipboard")
