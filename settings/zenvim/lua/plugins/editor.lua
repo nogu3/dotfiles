8fe927b97ff426c8175ba5a5e046dcf9afa77bf8
shoya.noguchi <nogukids@gmail.com>
2025-03-16 10:00:40 +0900

    Add preset for which-key.nvim to improve appearance

--- settings/zenvim/lua/plugins/editor.lua
+++ settings/zenvim/lua/plugins/editor.lua
@@ -102,6 +102,7 @@ return {
     "folke/which-key.nvim",
     event = "VeryLazy",
     opts = {
+      preset = "helix",
       plugins = { spelling = true },
       defaults = {
         mode = { "n", "v" },
