c9769663dc18ed8fab17bf448fdd9f8b7012ae50
shoya.noguchi <nogukids@gmail.com>
2025-12-02 21:24:55 +0900

    disable avante.nvim

--- settings/zenvim/lua/plugins/extras/ai/avante.lua
+++ settings/zenvim/lua/plugins/extras/ai/avante.lua
@@ -1,13 +1,16 @@
 return {
   "yetone/avante.nvim",
+  enabled = false,
   event = "VeryLazy",
   lazy = false,
   version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
   opts = {
-    provider = "copilot",
-    auto_suggestions_provider = "copilot",
+    -- provider is "copilot", "gemini"
+    provider = "gemini",
+    -- provider is "copilot"
+    -- auto_suggestions_provider = "copilot",
     behaviour = {
-      auto_suggestions = true,
+      -- auto_suggestions = true,
       auto_set_highlight_group = true,
       auto_set_keymaps = true,
       auto_apply_diff_after_generation = true,
@@ -27,51 +30,37 @@ return {
       },
     },
     file_selector = {
-      provider = "telescope",
-      -- Options override for custom providers
+      provider = "fzf-lua",
       provider_opts = {},
     },
 
     -- providers-setting
-    copilot = {
-      -- model = "gpt-4o",
-      -- model = "claude-3.7-sonnet",
-      model = "claude-3.5-sonnet",
-      max_tokens = 4096,
-      language = "ja",
+    providers = {
+      copilot = {
+        -- model is "gpt-4o", "claude-3.7-sonnet"
+        model = "claude-3.5-sonnet",
+        language = "ja",
+        extra_request_body = {
+          max_tokens = 4096,
+        },
+      },
+      gemini = {
+        -- see: model names
+        -- https://ai.google.dev/gemini-api/docs/models?hl=ja#model-variations
+        model = "gemini-3-pro-preview",
+        max_tokens = 4096,
+      },
     },
   },
 
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = "make",
-  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
     "stevearc/dressing.nvim",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
-    --- The below dependencies are optional,
-    "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-    -- "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-    "zbirenbaum/copilot.lua", -- for providers='copilot'
-    {
-      -- support for image pasting
-
-      "HakonHarnes/img-clip.nvim",
-      event = "VeryLazy",
-      opts = {
-        -- recommended settings
-        default = {
-          embed_image_as_base64 = false,
-          prompt_for_file_name = false,
-          drag_and_drop = {
-            insert_mode = true,
-          },
-          -- required for Windows users
-          use_absolute_path = true,
-        },
-      },
-    },
+    -- "zbirenbaum/copilot.lua", -- for providers='copilot'
     {
       -- Make sure to set this up properly if you have lazy=true
       "MeanderingProgrammer/render-markdown.nvim",
