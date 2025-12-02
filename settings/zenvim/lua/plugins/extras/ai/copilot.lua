039c039f6f08b40dd33e604ab2fa594f074d6e76
shoya.noguchi <nogukids@gmail.com>
2025-12-02 21:19:07 +0900

    delte unnessary comment

--- settings/zenvim/lua/plugins/extras/ai/copilot.lua
+++ settings/zenvim/lua/plugins/extras/ai/copilot.lua
@@ -1,30 +1,7 @@
+-- return {}
 return {
   {
     "github/copilot.vim",
-  },
-  -- FIXME lazy load when run nvim
-  {
-    "nvim-lualine/lualine.nvim",
-    optional = true,
-    event = "VeryLazy",
-    opts = function(_, opts)
-      local is_enabled = vim.fn["copilot#Enabled"]()
-      table.insert(opts.sections.lualine_x, 2, {
-        function()
-          if is_enabled then
-            return " "
-          else
-            return ":"
-          end
-        end,
-        color = function()
-          if is_enabled then
-            return { fg = 255 }
-          else
-            return { fg = 241 }
-          end
-        end,
-      })
-    end,
+    enabled = false,
   },
 }
