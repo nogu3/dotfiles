eaf89c22ce79d6165601a6dc6089e474a5f6a8f6
shoya-noguchi <nogukids@gmail.com>
2025-12-23 00:35:39 +0900

    add aqua and dotter

--- init.sh
+++ init.sh
@@ -12,27 +12,27 @@ rm -f ~/.tmux.conf
 ln -s "$SCRIPT_DIR/settings/tmux/.tmux.conf" ~/.tmux.conf
 
 # lazygit
-mkdir -p ~/.config/lazygit
-rm -f ~/.config/lazygit/config.yml
-ln -s "$SCRIPT_DIR/settings/lazygit/config.yml" ~/.config/lazygit/config.yml
+# mkdir -p ~/.config/lazygit
+# rm -f ~/.config/lazygit/config.yml
+# ln -s "$SCRIPT_DIR/settings/lazygit/config.yml" ~/.config/lazygit/config.yml
 
 # neovim
-mkdir -p ~/.config
-rm -f ~/.config/nvim
-ln -s "$SCRIPT_DIR/settings/zenvim" ~/.config/nvim
+# mkdir -p ~/.config
+# rm -f ~/.config/nvim
+# ln -s "$SCRIPT_DIR/settings/zenvim" ~/.config/nvim
 
 # zsh
 # common
-rm -f ~/.zshrc
-ln -s "$SCRIPT_DIR/settings/zsh/.zshrc" ~/.zshrc
+# rm -f ~/.zshrc
+# ln -s "$SCRIPT_DIR/settings/zsh/.zshrc" ~/.zshrc
 
 # specific
-rm -f ~/.zshrc_local
-ln -s "$SCRIPT_DIR/settings/zsh/.zshrc_local" ~/.zshrc_local
+# rm -f ~/.zshrc_local
+# ln -s "$SCRIPT_DIR/settings/zsh/.zshrc_local" ~/.zshrc_local
 
 # scripts
-rm -f ~/.scripts
-ln -s "$SCRIPT_DIR/.scripts" ~/.scripts
+# rm -f ~/.scripts
+# ln -s "$SCRIPT_DIR/.scripts" ~/.scripts
 
 if [[ $OS = "WINDOWS" ]]; then
   echo "copy to alacritty setting files for Windows"
