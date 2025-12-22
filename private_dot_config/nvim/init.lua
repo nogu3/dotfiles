b3b8263a1cdd1b0172301868c31b9d13033535e3
shoya-noguchi <nogukids@gmail.com>
2025-12-23 00:38:22 +0900

    delete chezmoi settings

--- private_dot_config/nvim/init.lua
+++
@@ -1,7 +0,0 @@
-_G.Zenvim = require("util")
-
--- HACK refactor auto require
-require("config.options")
-require("config.autocmds")
-require("config.lazy")
-require("config.keymaps")
--- private_dot_config/nvim/lua/config/autocmds.lua
+++
@@ -1,4 +0,0 @@
--- Highlight on yank
-Zenvim.set_auto_command("TextYankPost", "highlight_yank", function()
-  vim.highlight.on_yank()
-end)
